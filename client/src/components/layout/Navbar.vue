7baf212f12542ec17fef827c6799fd9565a06426
Santyyy <santiagoeseiza10@gmail.com>
2026-05-25 01:10:05 -0300

    feat: add authentication controls to navbar

--- client/src/components/layout/Navbar.vue
+++ client/src/components/layout/Navbar.vue
@@ -1,5 +1,29 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+import { useAuthStore } from '@/stores/auth'
+
+const router = useRouter()
+
+const authStore = useAuthStore()
+
+const handleLogout = () => {
+  authStore.logout()
+
+  router.push('/login')
+} // Maneja el proceso de cierre de sesión llamando a la función de logout del store de autenticación y redirigiendo al usuario a la página de login después de cerrar sesión
+</script>
+
 <template>
-  <header class="navbar">
+  <header>
     <h1>Navbar</h1>
+
+    <p>
+      {{ authStore.user }}
+    </p>
+
+    <button @click="handleLogout">
+      Logout
+    </button>
   </header>
 </template>
