ae92141565921712f5a782c74ce7528248119b60
Santyyy <santiagoeseiza10@gmail.com>
2026-05-25 01:53:50 -0300

    feat: implement frontend task management system

--- client/src/views/Tasks.vue
+++ client/src/views/Tasks.vue
@@ -1,3 +1,50 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import { useTasksStore } from '@/stores/tasks'
+
+const tasksStore = useTasksStore()
+
+const newTask = ref('')
+
+const handleAddTask = () => {
+  if (!newTask.value) return
+
+  tasksStore.addTask(newTask.value)
+
+  newTask.value = ''
+}
+</script>
+
 <template>
-  <h1>Tasks Page</h1>
+  <div>
+    <h1>Tasks</h1>
+
+    <input
+      v-model="newTask"
+      type="text"
+      placeholder="New task"
+    />
+
+    <button @click="handleAddTask">
+      Add Task
+    </button>
+
+    <ul>
+      <li
+        v-for="task in tasksStore.tasks"
+        :key="task.id"
+      >
+        {{ task.title }}
+
+        <button @click="tasksStore.toggleTask(task.id)">
+          Toggle
+        </button>
+
+        <button @click="tasksStore.removeTask(task.id)">
+          Delete
+        </button>
+      </li>
+    </ul>
+  </div>
 </template>
