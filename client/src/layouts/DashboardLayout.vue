b437b377c6bad77846a2bcaa08f9d61accf0f8f5
Santyyy <santiagoeseiza10@gmail.com>
2026-05-25 00:54:16 -0300

    feat: implement dashboard layout structure

---
+++ client/src/layouts/DashboardLayout.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Sidebar from '@/components/layout/Sidebar.vue'
+import Navbar from '@/components/layout/Navbar.vue'
+</script>
+
+<template>
+  <div class="dashboard-layout">
+    <Sidebar />
+
+    <div>
+      <Navbar />
+
+      <main>
+        <router-view />
+      </main>
+    </div>
+  </div>
+</template>
